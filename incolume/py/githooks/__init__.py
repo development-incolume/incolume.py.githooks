467e80a04e66ba748bfe0aed7461828ca438d535
Britodfbr <britodfbr@gmail.com>
2025-09-14 09:52:25 -0300

    fix: corrigido erro no acesso a versão do projeto

--- incolume/py/githooks/__init__.py
+++ incolume/py/githooks/__init__.py
@@ -1,1 +1,20 @@
 """Module githooks."""
+
+import contextlib
+import importlib.metadata
+import re
+from pathlib import Path
+from typing import Final
+
+import tomllib as tomli
+
+REGEX: Final = r'^\d+(\.\d+){2}((-\w+\.\d+)|(\w+\d+))?$'
+confproject = Path(__file__).parents[3] / 'pyproject.toml'
+fileversion = Path(__file__).parent / 'version.txt'
+
+with contextlib.suppress(FileNotFoundError), confproject.open('rb') as f:
+    fileversion.write_text(
+        f'{tomli.load(f)["project"]["version"]!s}\n',
+    )
+
+__version__ = fileversion.read_text().strip()
