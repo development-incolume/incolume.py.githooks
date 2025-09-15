417d03126be288cc92213524559d4cd2ab09de98
Britodfbr <britodfbr@gmail.com>
2025-09-15 08:47:05 -0300

    fix: #21 corrigido erro ao importar tomllib em Python3.10
    close #21

--- incolume/py/githooks/__init__.py
+++ incolume/py/githooks/__init__.py
@@ -1,12 +1,18 @@
 """Module githooks."""
+# ruff: noqa: E501
 
-import contextlib
-import importlib.metadata
-import re
+from __future__ import annotations
+
+from contextlib import suppress
 from pathlib import Path
 from typing import Final
 
-import tomllib as tomli
+with suppress(ImportError, ModuleNotFoundError):
+    import tomllib as tomli  # type: ignore[import]
+
+with suppress(ImportError, ModuleNotFoundError):
+    import tomli  # type: ignore[import]
+
 
 REGEX_SEMVER: Final = r'^\d+(\.\d+){2}((-\w+\.\d+)|(\w+\d+))?$'
 RULE_BRANCHNAME: Final = r'^((enhancement|feature|feat|bug|bugfix|fix|refactor)/(epoch|issue)#([0-9]+)|([0-9]+\-[a-z0-9\-]+))$'
@@ -15,7 +21,7 @@ RULE_COMMITFORMAT = r'^(((Merge|Bumping|Revert)|(bugfix|build|chore|ci|docs|feat
 confproject = Path(__file__).parents[3] / 'pyproject.toml'
 fileversion = Path(__file__).parent / 'version.txt'
 
-with contextlib.suppress(FileNotFoundError), confproject.open('rb') as f:
+with suppress(FileNotFoundError), confproject.open('rb') as f:
     fileversion.write_text(
         f'{tomli.load(f)["project"]["version"]!s}\n',
     )
