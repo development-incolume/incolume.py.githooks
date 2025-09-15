8e2af559ce49c9f124bc78a27776436f7c316c34
Britodfbr <britodfbr@gmail.com>
2025-09-14 21:26:43 -0300

    chore: #4 mapeamento local para hooks implementados
    close #12
    
    close #14
    
    exemplos uteis bash e python2.7 disponíveis em https://www.atlassian.com/br/git/tutorials/git-hooks
    
    close #1

---
+++ incolume/py/githooks/pre_commit_running.py
@@ -0,0 +1,19 @@
+"""Verify if pre-commit is installed."""
+
+from pathlib import Path
+
+from colorama import Fore, Style
+
+
+def run() -> int:
+    """Run it."""
+    if not Path('.pre-commit-config.yaml').exists():
+        print(
+            f'{Fore.RED}pre-commit configuration detected, but `pre-commit install` was never run{Style.RESET_ALL}',
+        )
+        return 1
+    return 0
+
+
+if __name__ == '__main__':  # pragma: no cover
+    raise SystemExit(run())
