be2ccb70d52e7cbf87a8207f50b39626f51100cc
Britodfbr <britodfbr@gmail.com>
2025-09-14 22:11:14 -0300

    fix: correção no encode para processamento adequado

--- incolume/py/githooks/pre_commit.py
+++ incolume/py/githooks/pre_commit.py
@@ -1,31 +1,36 @@
 """Module for check branchname."""
 
-# ruff: noqa: T201 E501
+# ruff: noqa: T201
 
 import logging
 import platform
 import re
 import subprocess
 import sys
+
 from colorama import Fore, Style
+
 from incolume.py.githooks import RULE_BRANCHNAME
 
-
-BRANCH = subprocess.check_output(
-    ['git', 'rev-parse', '--abbrev-ref', 'HEAD'],
-).strip()
+BRANCH = (
+    subprocess.check_output(
+        ['git', 'rev-parse', '--abbrev-ref', 'HEAD'],
+    )
+    .strip()
+    .decode('utf-8')
+)
 
 
 def run() -> None:
     """Run it."""
     logging.debug(platform.python_version_tuple())
-    result = f'{Fore.GREEN}branching name rules. [OK]\033[0m'
+    result = f'{Fore.GREEN}Branching name rules. [OK]{Style.RESET_ALL}'
     status = 0
     if not re.match(RULE_BRANCHNAME, BRANCH):
         result = (
             f'{Fore.RED}Your commit was rejected due to branching name '
-            f'incompatible with rules.{Style.RESET_ALL}\n'
-            f"{Fore.RED}Please rename your branch with '<(enhancement|feature|feat"
+            'incompatible with rules.\n'
+            "Please rename your branch with '<(enhancement|feature|feat"
             f"|bug|bugfix|fix)>/epoch#<timestamp>' syntax{Style.RESET_ALL}"
         )
         status = 1
