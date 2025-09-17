47098604a0b748cc0a1d79d393b58e0999be7491
Britodfbr <britodfbr@gmail.com>
2025-09-17 16:22:50 -0300

    test: #26 estrutura de testes para prepare_commit_msg
    close #25

--- tests/prepare_commit_msg_test.py
+++ tests/prepare_commit_msg_test.py
@@ -2,6 +2,12 @@
 
 from typing import NoReturn
 import pytest
+from incolume.py.githooks.prepare_commit_msg import (
+    MESSAGERROR,
+    MESSAGESUCCESS,
+    prepend_commit_msg,
+)
+from icecream import ic
 
 
 class TestCasePrepareCommitMsg:
@@ -11,6 +17,21 @@ class TestCasePrepareCommitMsg:
         ['entrance', 'expected'],
         [
             pytest.param(
+                MESSAGERROR,
+                '',
+                marks=[pytest.mark.xfail(reason='Test not implemented yet')],
+            ),
+            pytest.param(
+                MESSAGESUCCESS,
+                '',
+                marks=[pytest.mark.xfail(reason='Test not implemented yet')],
+            ),
+            pytest.param(
+                prepend_commit_msg(),
+                '',
+                marks=[pytest.mark.xfail(reason='Test not implemented yet')],
+            ),
+            pytest.param(
                 '',
                 '',
                 marks=[pytest.mark.xfail(reason='Test not implemented yet')],
@@ -19,7 +40,7 @@ class TestCasePrepareCommitMsg:
     )
     def test_messages(self, entrance, expected) -> NoReturn:
         """Test messages."""
-        assert entrance == expected
+        assert ic(entrance) == expected
 
     @pytest.mark.xfail(reason='Test not implemented yet')
     def test_prepend_commit_msg(self) -> NoReturn:
