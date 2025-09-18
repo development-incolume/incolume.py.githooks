a01af376bacfd94dc737f65a3fa1d05a5d555159
Britodfbr <britodfbr@gmail.com>
2025-09-18 09:38:26 -0300

    refactor: #34 Transformado em testável

--- incolume/py/githooks/commit_msg.py
+++ incolume/py/githooks/commit_msg.py
@@ -1,9 +1,18 @@
 """Module to handle commit message hook."""
 
+from __future__ import annotations
+
 import secrets
 import sys
+from typing import Final
 
 import rich
+
+MESSAGES: Final[str] = [
+    'Boa! Continue o bom trabalho com a força, Jedi!',
+    'Boa! Continue trabalhando campeão!',
+    'Executado com sucesso.',
+]
 
 
 def check() -> None:
@@ -12,13 +21,9 @@ def check() -> None:
     rich.print(f'Arguments List: {sys.argv!s}')
 
 
-def get_msg(*, fixed: bool = False) -> None:
+def get_msg(*, fixed: bool = False, messages: list[str] | None = None) -> None:
     """Get message."""
-    messages = [
-        'Boa! Continue o bom trabalho com a força, Jedi!',
-        'Boa! Continue trabalhando campeão!',
-        'Executado com sucesso.',
-    ]
+    messages = messages or MESSAGES
     msg = messages[0] if fixed else secrets.choice(messages)
     result = f'\n[green]{msg}[/]\n'
 
