d74bc6649e096552400ec714e4505a347ea8a759
Britodfbr <britodfbr@gmail.com>
2025-09-21 12:17:51 -0300

    perf: Melhoria da lógica aplicada a regra de negócio

--- incolume/py/githooks/pre_commit_running.py
+++ incolume/py/githooks/pre_commit_running.py
@@ -1,20 +1,26 @@
 """Verify if pre-commit is installed."""
-# ruff: noqa: T201
 
+# ruff: noqa: T201
+from __future__ import annotations
+
+import sys
 from pathlib import Path
 
 from colorama import Fore, Style
 
+from incolume.py.githooks import FAILURE, SUCCESS
+
 
 def run() -> int:
     """Run it."""
+    result = SUCCESS
     if not Path('.pre-commit-config.yaml').exists():
         print(
             f'{Fore.RED}pre-commit configuration detected,'
             f' but `pre-commit install` was never run{Style.RESET_ALL}',
         )
-        return 1
-    return 0
+        result |= FAILURE
+    return sys.exit(result)
 
 
 if __name__ == '__main__':  # pragma: no cover
