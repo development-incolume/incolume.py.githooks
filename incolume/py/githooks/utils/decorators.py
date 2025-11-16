dfdf12c5e86ee1005c8be3fac45630758e79e8d4
Britodfbr <britodfbr@gmail.com>
2025-11-16 15:29:52 -0300

    feat: #126 implementação do decorador para eventos criticos
    close #128

---
+++ incolume/py/githooks/utils/decorators.py
@@ -0,0 +1,49 @@
+"""Decorators module for githooks utilities."""
+
+from __future__ import annotations
+
+import logging
+from functools import wraps
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+from icecream import ic
+
+from . import debug_enable, debug_var_active
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
+
+debug_enable()
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename=Path(__file__).parents[4] / 'logs' / 'githooks.log',
+    filemode='a',
+    encoding='utf-8',
+)
+
+
+def critical_log_call(func: Callable) -> Callable:
+    """Decoratore to debug function calls."""
+
+    @wraps(func)
+    def wrapper(*args: str, **kwargs: dict) -> None:
+        """Wrapp function to add logging critical."""
+        debug: bool = debug_var_active()
+
+        if debug:
+            ic.enable()
+            ic(f'Calling function: {func.__name__}')
+            ic(f'Arguments: {args}, {kwargs}')
+
+        result = func(*args, **kwargs)
+
+        if debug:
+            logging.critical(
+                ic(f'Function {func.__name__} called with critial status.')
+            )
+
+        return result
+
+    return wrapper
