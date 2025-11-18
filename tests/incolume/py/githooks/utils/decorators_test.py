6cb68db18fe5aab292c43ac44249309f2bc73879
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-11-18 09:36:51 -0300

    test: #126 tratativa em direcionar registro de log para arquivo de testes
    baseado no exemplo oficial disponível em https://docs.pytest.org/en/stable/how-to/logging.html

--- tests/incolume/py/githooks/utils/decorators_test.py
+++ tests/incolume/py/githooks/utils/decorators_test.py
@@ -1,11 +1,8 @@
 """Test module for decorators."""
 
 import logging
-from pathlib import Path
 import pytest
 from incolume.py.githooks.utils import decorators
-from tempfile import NamedTemporaryFile
-from icecream import ic
 
 
 class TestCaseDecorators:
@@ -39,13 +36,3 @@ class TestCaseDecorators:
 
             assert result == entrance
             assert [rec.message for rec in caplog.records] == expected
-
-        with (
-            NamedTemporaryFile(delete=False, suffix='.log') as logger,
-            caplog.at_level(logging.DEBUG, logger=logger.name),
-        ):
-            caplog.clear()
-            sample_function(entrance)
-
-            ic(logger.name)
-            assert Path(logger.name).read_text(encoding='utf-8') == 'abc'
