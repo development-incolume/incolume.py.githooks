07c2d8b75bbef27c0526314257483e95646e3f3d
Brito <britodfbr@gmail.com>
2026-08-24 10:39:16 -0300

    perf: #85 captura chaveada com proteção de falha

--- tests/incolume/py/githooks/validate_filename_test.py
+++ tests/incolume/py/githooks/validate_filename_test.py
@@ -421,7 +421,7 @@ class TestCaseValidFilename:
         self, entrance: Mapping[str, str], expected: Result
     ) -> None:
         """Test invalid filenames."""
-        fout = self.test_dir / stack()[0][3] / entrance['filename']
+        fout = self.test_dir / stack()[0][3] / entrance.get('filename', '')
         fout.parent.mkdir(parents=True, exist_ok=True)
         vf = ValidateFilename(filename=fout)
         result = vf.is_valid()
