37683fcc2df82ff28dc9a636a54467ea5e9252cf
Ricardo Brito do Nascimento <britodfbr@gmail.com>
2026-08-29 10:17:05 -0300

    refactor: #175 atualzado a regra para snake_case

--- incolume/py/githooks/core/rules.py
+++ incolume/py/githooks/core/rules.py
@@ -246,7 +246,7 @@ RULE_BRANCHNAME: Final[str] = (
 RULE_COMMITFORMAT: Final[str] = (
     r'^(((Merge|Bumping|Revert)|(bugfix|build|chore|ci|docs|feat|feature|fix|other|perf|refactor|revert|style|test)(\(.*\))?\!?: #[0-9]+) .*(\n.*)*)$'
 )
-SNAKE_CASE: Final[str] = r'^[a-z_][a-z_0-9]+$'
+SNAKE_CASE: Final[str] = r'^[a-z_0-9]+$'
 
 MESSAGES: Final[list[str]] = [
     'Boa! Continue o bom trabalho com a força, Jedi!',
