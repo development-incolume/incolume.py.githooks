c07da105fd62f1b786377302291d91166d507a20
Brito <britodfbr@gmail.com>
2026-09-04 07:48:31 -0300

    ci: #187 acrescentado flag nocover para test_my_decorator

--- tests/incolume/py/githooks/core/decorators_test.py
+++ tests/incolume/py/githooks/core/decorators_test.py
@@ -29,7 +29,7 @@ class TestCaseDecorators:
         debug_enable()
 
     @pytest.mark.noci
-    def test_my_decorator(self, capsys: pytest.CaptureFixture[str]) -> None:
+    def test_my_decorator(self, capsys: pytest.CaptureFixture[str]) -> None:   # pragma: no cover
         """Test para decorador my-decorator."""
         expected = (
             'Before function "sample_function" call',
@@ -48,7 +48,7 @@ class TestCaseDecorators:
     @pytest.mark.noci
     def test_simple_decorator(
         self, capsys: pytest.CaptureFixture[str]
-    ) -> None:
+    ) -> None:   # pragma: no cover
         """Test para decorador my-decorator."""
         expected = (
             'Debug mode enabled.',
