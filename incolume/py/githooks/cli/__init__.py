5dcad9c0abe6d080160fe205b7dfc1afe238f2d2
Brito <britodfbr@gmail.com>
2026-09-14 09:16:12 -0300

    refactor: #202 configurados os modulos do pacote `cli`

---
+++ incolume/py/githooks/cli/__init__.py
@@ -0,0 +1,31 @@
+"""Module githooks.cli."""
+
+from incolume.py.githooks.cli.cli import (
+    check_len_first_line_commit_msg_cli,
+    check_type_commit_msg_cli,
+    check_valid_branchname_cli,
+    check_valid_filenames_cli,
+    clean_commit_msg_cli,
+    detect_private_key_cli,
+    effort_msg_cli,
+    footer_signedoffby_cli,
+    get_msg_cli,
+    insert_diff_cli,
+    pre_commit_installed_cli,
+    validate_format_commit_msg_cli,
+)
+
+__all__ = [
+    'check_len_first_line_commit_msg_cli',
+    'check_type_commit_msg_cli',
+    'check_valid_branchname_cli',
+    'check_valid_filenames_cli',
+    'clean_commit_msg_cli',
+    'detect_private_key_cli',
+    'effort_msg_cli',
+    'footer_signedoffby_cli',
+    'get_msg_cli',
+    'insert_diff_cli',
+    'pre_commit_installed_cli',
+    'validate_format_commit_msg_cli',
+]
