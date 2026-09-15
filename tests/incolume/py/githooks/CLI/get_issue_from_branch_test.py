bd16103fdeff5813b85f7513ac3f29dfd1358c49
Brito <britodfbr@gmail.com>
2026-09-15 11:15:28 -0300

    test: #195 configuração fixture escopo de class

--- tests/incolume/py/githooks/CLI/get_issue_from_branch_test.py
+++ tests/incolume/py/githooks/CLI/get_issue_from_branch_test.py
@@ -9,37 +9,39 @@ from pathlib import Path
 from tempfile import NamedTemporaryFile, gettempdir
 from inspect import stack
 
+@pytest.fixture(scope='class')
+def filefortest(request) -> Generator[Path, None, None]:
+    """Get the path to this file."""
+    request.cls.test_dir.mkdir(parents=True, exist_ok=True)
+    with NamedTemporaryFile(dir=request.cls.test_dir) as tf:
+        filename = Path(tf.name)
+    filename.parent.mkdir(parents=True, exist_ok=True)
+    filename.touch(exist_ok=True)
+    yield filename
+    shutil.rmtree(filename, ignore_errors=True)
+
 
 class TestCaseGetIssueFromBranch:
     """Test cases CLI main for get_issue_from_branch function."""
 
     test_dir = Path(gettempdir()) / stack()[0][3]
 
-    @pytest.fixture(scope='class')
-    def filefortest(self) -> Generator[Path, None, None]:
-        """Get the path to this file."""
-        self.test_dir.mkdir(parents=True, exist_ok=True)
-        with NamedTemporaryFile(dir=self.test_dir) as tf:
-            filename = Path(tf.name)
-        filename.parent.mkdir(parents=True, exist_ok=True)
-        filename.touch(exist_ok=True)
-        yield filename
-        shutil.rmtree(filename, ignore_errors=True)
+
 
     @pytest.mark.parametrize(
         ['entrance', 'expected'],
         [
             pytest.param(['', '', ''], '[ISSUE-195] ', marks=[]),
         ],
+        scope='class',
     )
     def test_get_issue_from_branch(
         self,
-        filefortest: Generator[Path, None, None],
         entrance: list[str],
         expected: str,
     ) -> None:
         """Test get_issue_from_branch function."""
-        flname = next(filefortest)
+        flname = filefortest
         entrance[1] = str(flname)
         main(entrance)
         assert flname.read_text() == expected
