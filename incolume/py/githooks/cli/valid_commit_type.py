d5f156e1996d9037d0141a2dbd5c2b9f0d3cae22
Brito <britodfbr@gmail.com>
2026-09-16 10:48:30 -0300

    style: #207 código fatorado

--- incolume/py/githooks/cli/valid_commit_type.py
+++ incolume/py/githooks/cli/valid_commit_type.py
@@ -16,18 +16,22 @@ def run(argv: Sequence[str] | None = None) -> int:
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        'commit_msg_file', default='', help='Arquivo contendo a mensagem do commit'
+        'commit_msg_file',
+        default='',
+        help='Arquivo contendo a mensagem do commit',
     )
     args = parser.parse_args(argv)
     commit_msg_file = args.commit_msg_file
 
-    commit_msg = pathlib.Path(commit_msg_file).read_text(encoding='utf-8').strip()
+    commit_msg = (
+        pathlib.Path(commit_msg_file).read_text(encoding='utf-8').strip()
+    )
 
-    if not commit_msg.startswith("feat") and not commit_msg.startswith("fix"):
-        secho("Erros: Mensagens devem começar com 'feat' ou 'fix'", fg="red")
+    if not commit_msg.startswith('feat') and not commit_msg.startswith('fix'):
+        secho("Erros: Mensagens devem começar com 'feat' ou 'fix'", fg='red')
         return 1
 
-    secho("Mensagem validada com sucesso.", fg="green")
+    secho('Mensagem validada com sucesso.', fg='green')
     return 0
 
 
