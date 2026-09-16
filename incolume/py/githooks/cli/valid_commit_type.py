cf477a6d68d07481dc212cc64ae35f8161d0417d
Brito <britodfbr@gmail.com>
2026-09-16 10:41:27 -0300

    refactor: #207 adaptação para biblioteca argparse

--- incolume/py/githooks/cli/valid_commit_type.py
+++ incolume/py/githooks/cli/valid_commit_type.py
@@ -1,14 +1,35 @@
-#!/usr/bin/env python
+"""Module to validate commit messages."""
+
+import argparse
+import inspect
+import pathlib
 import sys
+from collections.abc import Sequence
 
-commit_msg_file = sys.argv[1]
+from click import secho
+from icecream import ic
 
-with open(commit_msg_file, "r") as f:
-    commit_msg = f.read()
 
-if not commit_msg.startswith("feat") and not commit_msg.startswith("fix"):
-    print("Erros: Mensagens devem começar com 'feat' ou 'fix'")
-    sys.exit(1)
+def run(argv: Sequence[str] | None = None) -> int:
+    """Validate commit messages."""
+    ic(f'{inspect.currentframe().f_code.co_name}: {sys.argv=}, {argv=}')
 
-print("Mensagem validada com sucesso.")
-sys.exit(0)   
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'commit_msg_file', default='', help='Arquivo contendo a mensagem do commit'
+    )
+    args = parser.parse_args(argv)
+    commit_msg_file = args.commit_msg_file
+
+    commit_msg = pathlib.Path(commit_msg_file).read_text(encoding='utf-8').strip()
+
+    if not commit_msg.startswith("feat") and not commit_msg.startswith("fix"):
+        secho("Erros: Mensagens devem começar com 'feat' ou 'fix'", fg="red")
+        return 1
+
+    secho("Mensagem validada com sucesso.", fg="green")
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(run(sys.argv[1:]))
