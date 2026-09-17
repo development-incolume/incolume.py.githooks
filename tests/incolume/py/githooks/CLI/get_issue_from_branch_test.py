708fab549df18d7b8111de71e954bd1af7c78183
Brito <britodfbr@gmail.com>
2026-09-17 10:42:33 -0300

    refactor: #213 fatorado os testes CLI, implantados mock, e validado exit-code
    close #213

--- tests/incolume/py/githooks/CLI/get_issue_from_branch_test.py
+++ tests/incolume/py/githooks/CLI/get_issue_from_branch_test.py
@@ -2,12 +2,14 @@
 
 from collections.abc import Generator
 import shutil
-
+from incolume.py.githooks.core import subprocess
+from unittest.mock import patch
 from incolume.py.githooks.cli.get_issue_from_branch import run
 import pytest
 from pathlib import Path
 from tempfile import NamedTemporaryFile, gettempdir
 from inspect import stack
+from click.testing import CliRunner
 
 
 @pytest.fixture(scope='class')
@@ -30,25 +32,48 @@ class TestCaseGetIssueFromBranch:
     @pytest.mark.parametrize(
         ['entrance', 'expected'],
         [
-            pytest.param(['', '', ''], '[ISSUE-195] ', marks=[]),
             pytest.param(
-                ['', test_dir / 'COMMIT_EDITMSG', ''], '[ISSUE-195] ', marks=[]
+                ['', ''],
+                '[ISSUE-123] ',
+                marks=[
+                    pytest.mark.runner_setup(
+                        charset='utf-8', env={'test': 1}, echo_stdin=True
+                    )
+                ],
+            ),
+            pytest.param(
+                [test_dir / 'COMMIT_EDITMSG', ''],
+                '[ISSUE-123] ',
+                marks=[
+                    pytest.mark.runner_setup(
+                        charset='cp1251', env={'test': 2}, echo_stdin=True
+                    )
+                ],
             ),
         ],
         scope='class',
     )
     def test_get_issue_from_branch(
         self,
+        isolated_cli_runner: CliRunner,
         filefortest: Path,
         entrance: list[str],
         expected: str,
     ) -> None:
         """Test get_issue_from_branch function."""
         flin: Path = filefortest
-        if entrance[1]:
-            flin = Path(entrance[1])
+        if entrance[0]:
+            flin = Path(entrance[0])
             flin.parent.mkdir(parents=True, exist_ok=True)
             flin.touch(exist_ok=True)
-        entrance[1] = str(flin)
-        run(entrance)
-        assert flin.read_text(encoding='utf-8') == expected
+        entrance[0] = str(flin)
+
+        with patch.object(
+            subprocess,
+            'check_output',
+            return_value=bytes('123-fake-commit-message', 'utf-8'),
+        ):
+            result = isolated_cli_runner.invoke(run, entrance)
+            assert result.exit_code == 0
+            # assert 'Adicionado o número do ticket 123 à mensagem de commit.' in result.output
+            assert flin.read_text(encoding='utf-8') == expected
